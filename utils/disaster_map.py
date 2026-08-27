9bc58cbad558e733a667ea28bc293664a34ad22c
Ayush Jain <ayushjain8923@gmai.com>
2026-08-27 12:40:16 +0530

    checking the map issue-22

--- utils/disaster_map.py
+++ utils/disaster_map.py
@@ -622,7 +622,7 @@ def create_disaster_spatial_map(
                         "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
                     ],
                     below="traces",
-                    opacity=0.05
+                    opacity=1
                 )
             ],
             zoom=map_view["zoom"],
